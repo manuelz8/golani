79e3ec6160fed06f742bd87a14d7d2974f7e15bb
Manuel Zambrano <89891295+manuelz8@users.noreply.github.com>
2023-10-24 01:19:26 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+var video = document.getElementById("backgroundVideo");
+    
+// Función para verificar si el dispositivo es un iPhone o iPad
+function isiOS() {
+    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+}
+
+// Verificar si es un dispositivo iOS y desactivar el autoplay
+if (isiOS()) {
+    video.removeAttribute("autoplay");
+}
+        
+        
         // Obtenemos una referencia al video
         var video = document.getElementById("backgroundVideo");
 
